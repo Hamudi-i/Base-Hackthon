73e1999c14828a36086d4de88fe0940417e60d27
Mahammed <mahmmedsadik3@gmail.com>
2025-05-16 23:25:26 +0300

    Saved the changes on VS code this time around

--- components/WalletPopup.tsx
+++ components/WalletPopup.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import { useState } from 'react';
 
 export default function WalletPopup({ onClose }: { onClose: () => void }) {
   const connectWallet = (type: string) => {
